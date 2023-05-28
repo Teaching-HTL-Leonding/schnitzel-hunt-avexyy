d8fe720cc46659ed3c2bc4a255b91a0a25e87002
avexyy <113837032+avexyy@users.noreply.github.com>
2023-05-28 16:56:35 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿System.Console.WriteLine("Where to get Schnitzel");
+System.Console.WriteLine("======================");
+
+var files = Directory.GetFiles(@"C:\HTL\1AHIF\Programmieren\SchnitzelHuntFiles");
+
+foreach (var file in files)
+{
+    var lines = File.ReadAllLines(file);
+    var firstSchnitzelFound = false;
+    foreach (var line in lines)
+    {
+        var splittedLine = line.Split(':');
+        var dish = splittedLine[0];
+        if (!line.Contains(":")) { continue; }
+        if (dish.Contains("Schnitzel"))
+        {
+            if (!firstSchnitzelFound)
+            {
+                System.Console.WriteLine(Path.GetFileNameWithoutExtension(file));
+                firstSchnitzelFound = true;
+            }
+            System.Console.Write("   ");
+            System.Console.WriteLine(dish);
+        }
+    }
+}
